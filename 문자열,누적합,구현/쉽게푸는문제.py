c461852a719113a09f8f1b876928cdcf59bb7a2a
Junwu Park <63538435+pigpiano@users.noreply.github.com>
2024-03-28 18:20:25 +0900

    Add files via upload

---
+++ 문자열,누적합,구현/쉽게푸는문제.py
@@ -0,0 +1,13 @@
+
+a, b = map(int, input().split())
+
+data = [0]
+sum = 0
+
+for i in range(1, b+1):
+	for j in range(i): # 숫자에 해당하는 만큼 반복해서 더한다.
+		data.append(i)
+for i in range(a, b+1):
+	sum += data[i]
+print(sum)
+
